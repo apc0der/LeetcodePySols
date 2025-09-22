592455a3fa5f69cb5501aa8b30bb09f6706f7e10
Amogha Pokkulandra <69405645+apc0der@users.noreply.github.com>
2025-09-22 13:31:12 -0500

    Update 3170_LexicographicallyMinimumStringAfterRemovingStars.py

--- 3170_LexicographicallyMinimumStringAfterRemovingStars.py
+++ 3170_LexicographicallyMinimumStringAfterRemovingStars.py
@@ -1,14 +1,12 @@
 class Solution:
     def clearStars(self, s: str) -> str:
         minheap = [] # sort by smallest lex, then by rightmost index
-        starred = set() # ignore stars at the end
 
         for idx in range(len(s)):
             if s[idx] == '*':
-                starred.add(idx) # no stars
                 tup = heappop(minheap)
-                starred.add(-1*tup[1]) # no deleted chars
             else:
                 heappush(minheap, (s[idx], -1*idx)) # ascending lex, descending index
-
-        return "".join([s[x] for x in range(len(s)) if x not in starred])
+        
+        minheap.sort(key=lambda c: (-c[1]))
+        return "".join([c[0] for c in minheap])
